d65cceef1d61ce3fa7b8af90e9f62db790251d8c
thePrimativ <36805611+thePrimativ@users.noreply.github.com>
2020-07-07 10:30:38 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ for (let index = 0; index < 9000000; index++) {
     music.setVolume(113)
     music.playMelody("C5 B A G F E D C ", 120)
     music.playMelody("C D E F G A B C5 ", 120)
+    music.playMelody("C F C5 B A E D G ", 120)
 }
